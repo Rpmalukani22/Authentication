e306d02c9bd7c8b88f4403a4da2a6c9de5071749
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-11-16 16:59:26 +0530

    Authentication
    Authentication page using nodejs(passportjs) and mongodb.

--- app.js
+++ app.js
@@ -7,25 +7,36 @@ var localStrategy=require("passport-local");
 var app = express();
 var expressSession=require("express-session");
 
+
 app.set("view engine","ejs");
 mongoose.connect("mongodb://localhost/users",{ useNewUrlParser: true });
-app.use(passport.initialize());
-app.use(passport.session());
+
 app.use(expressSession({
     secret:"Secret text",
     resave:false,
     saveUninitialized:false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req,res,next)=>{
+    if(req.user)
+    res.locals.currentUser=req.user.username;
+    else
+    res.locals.currentUser=undefined;
+    next();
+});
 app.get("/",(req,res)=>{
     res.render("home");
 });
 
-app.get("/main",(req,res)=>{
-    res.render("main");
+app.get("/main",isLoggedIn,(req,res)=>{
+    res.render("main",{username:req.query.username});
 });
 
 app.get("/register",(req,res)=>{
@@ -40,16 +51,49 @@ app.post("/register",(req,res)=>{
  User.register(new User({username:req.body.username}),req.body.password,(err,user)=>{
      if(err){
          console.log(err);
-         return res.render('register');
+         return res.send("<h1>"+err.message+"</h1> <a href='login'> login here</a>");
      }
      else{
          passport.authenticate("local")(req,res,()=>{
-             res.redirect('main');
+             res.redirect('/main');
          });
      }
  });
  
 });
-app.listen(process.env.PORT,process.env.IP,()=>{
-    console.log("Server started!\nPORT:",process.env.PORT);
+app.get("/login",(req,res)=>{
+    res.render('login');
+});
+
+app.post('/login',(req, res, next) =>{
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (! user) {
+      return res.send('<h1>authentication failed.</h1> <a href="login">Try again</a><br><a href="register">Register Now!</a>');
+    }
+    req.login(user, loginErr => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+      return res.redirect('/main');
+    });      
+  })(req, res, next);
+});
+
+app.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req,res,next){
+    console.log(req.isAuthenticated());
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+app.listen(3000||process.env.PORT,process.env.IP,()=>{
+    console.log("Server started!");
 });
